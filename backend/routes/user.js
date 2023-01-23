380746d38e13bad618d2a77873c04a33c8ee0de0
Hindsedrati <92885675+Hindsedrati@users.noreply.github.com>
2023-01-23 21:23:52 +0100

    Update user.js
    Signup/Login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -5,7 +5,7 @@ const userCtrl = require('../controllers/user');
 //const password = require('../middleware/password')
 
 
-router.post('/signup', /*password*/ userCtrl.signup);
+router.post('/signup', userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
