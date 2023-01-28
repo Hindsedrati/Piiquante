b119b62c45f3f77b6adf1de61086cc9acd2d4b63
Hindsedrati <92885675+Hindsedrati@users.noreply.github.com>
2023-01-28 08:32:12 +0100

    Update sauces.js

--- backend/routes/sauces.js
+++ backend/routes/sauces.js
@@ -1,9 +1,14 @@
 const express = require('express');
+//le module "express" pour créer un routeur
 const router = express.Router();
 
 const saucesCtrl = require('../controllers/sauces');
 const auth = require('../middleware/auth');
+//"auth" nous permets de s'assurer que l'utilisateur est authentifié avant de 
+//pouvoir effectuer les actions
 const multer = require('../middleware/multer-config');
+//"multer" pour gérer les images associées aux sauces
+
 
 router.post('/', auth, multer, saucesCtrl.createSauce);
 router.post('/:id/like', auth, saucesCtrl.likeSauce);
