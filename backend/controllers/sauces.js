fcbda29361721a2805f338ddf6be4f6a71f47eb6
Hindsedrati <92885675+Hindsedrati@users.noreply.github.com>
2023-01-28 08:27:59 +0100

    Update sauces.js

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -1,7 +1,12 @@
 const Sauce = require('../models/modelsSauce');
+//le module 'fs' pour supprimer les images associées à une sauce lorsque celle-ci 
+//est supprimée
 const fs = require('fs');
 
 // --------- CRÉER/ENREGISTRER UNE SAUCE ----------
+// La création d'une sauce en utilisant les données envoyées dans la requête, 
+//en enregistrant également l'image associée à cette sauce.
+
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce)
     delete sauceObject._id;
@@ -15,18 +20,23 @@ exports.createSauce = (req, res, next) => {
 };
 
 // --------- MODIFIER UNE SAUCE ----------
+//La modification d'une sauce en utilisant les données envoyées dans la requête
+
 exports.modifySauce = (req, res, next) => {
     const sauceObject = req.file ?
         {
             ...JSON.parse(req.body.sauce),
             imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
         } : { ...req.body};
+// mettant à jour l'image associée à cette sauce si elle est incluse dans la requête
     Sauce.updateOne({ _id: req.params.id, userId: req.auth.userId}, { ...sauceObject, _id: req.params.id })
         .then(() => res.status(200).json({ message: 'Sauce modifié !'}))
         .catch(error => res.status(400).json({ error }));
 };
 
 // --------- SUPPRIMER UNE SAUCE ----------
+// La suppression d'une sauce supprime également l'image associée à cette sauce.
+
 exports.deleteSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id, userId: req.auth.userId}).then
         (sauce => {
@@ -36,7 +46,9 @@ exports.deleteSauce = (req, res, next) => {
                     error: new Error('No such Sauce!')
                 });
             }
-            // ----------FIN empecher n'importe quel utilisateur de supprimer une sauce --------- 
+            
+            // ----------empecher n'importe quel utilisateur de supprimer une sauce --------- 
+            // "Split" pour tout récupérer après l'espace 
             const filename = sauce.imageUrl.split('/images/')[1];
             fs.unlink(`images/${filename}`, () => {
                 Sauce.deleteOne({ _id: req.params.id })
@@ -48,6 +60,9 @@ exports.deleteSauce = (req, res, next) => {
 };
 
 // --------- RÉCUPÉRER UNE SAUCE ----------
+//La récupération d'une sauce spécifique en utilisant l'ID de la sauce 
+//dans la requête.
+
 exports.getOneSauce = (req, res, next) => {
     Sauce.findOne({ _id: req.params.id })
         .then(sauce => res.status(200).json(sauce))
@@ -55,6 +70,8 @@ exports.getOneSauce = (req, res, next) => {
 };
 
 // --------- RÉCUPÉRER L'ENSEMBLE DES SAUCES ----------
+//La récupération de toutes les sauces enregistrées.
+
 exports.getAllSauces = (req, res, next) => {
     Sauce.find()
         .then(sauces => res.status(200).json(sauces))
@@ -62,6 +79,9 @@ exports.getAllSauces = (req, res, next) => {
 };
 
 // --------- AVIS DONNER AUX SAUCES -------
+// l'ajout d'avis sur une sauce en utilisant l'ID de la sauce dans la requête 
+//et en actualisant le compteur des likes ou dislikes pour cette sauce.
+
 exports.likeSauce = (req, res, next) => {
     const like = req.body.like;
     const idSauce = req.params.id;
