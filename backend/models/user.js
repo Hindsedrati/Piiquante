874e9d3a23180377a5c7fc12916a05098d176879
Hindsedrati <92885675+Hindsedrati@users.noreply.github.com>
2023-01-28 08:31:41 +0100

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose');
 
 const uniqueValidator = require('mongoose-unique-validator');
-
+//module "mongoose-unique-validator" pour valider l'unicité de l'email
 const userSchema = mongoose.Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true }
 });
-
+//Ce modèle sera utilisé pour créer et manipuler des documents de données 
+//d'utilisateurs dans la base de données MongoDB
 userSchema.plugin(uniqueValidator);
 
 module.exports = mongoose.model('User', userSchema);
