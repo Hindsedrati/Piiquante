b5bab03f4b94848b32e5363d36e38fa763624752
Hindsedrati <92885675+Hindsedrati@users.noreply.github.com>
2023-01-28 08:30:44 +0100

    Update multer-config.js

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,6 +1,7 @@
 //----- Implémenter des téléchargements de fichiers -----
 const multer = require('multer');
 
+//"MIME_TYPES" mappe les différents types de fichiers image à leurs extensions de fichier.
 const MIME_TYPES = {
     'image/jpg': 'jpg',
     'image/jpeg': 'jpg',
@@ -9,6 +10,8 @@ const MIME_TYPES = {
 
 //------ configuration de multer -------
 const storage = multer.diskStorage({
+//configuration pour enregistrer les fichiers téléchargés dans un dossier appelé "images" 
+//sur le disque du serveur.
     destination: (req, file, callback) => {
         callback(null, 'images')
     },
@@ -18,5 +21,6 @@ const storage = multer.diskStorage({
         callback(null, name + Date.now() + '.' + extension);
     }
 });
-
+//configuration avec l'objet de stockage créé, et configuré pour gérer un seul fichier avec 
+//le nom de champ "image"
 module.exports = multer({ storage }).single('image');
