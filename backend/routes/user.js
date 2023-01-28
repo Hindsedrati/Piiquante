8029e43e97d1adb8ca5b306b1b4b69a121dcc7b9
Hindsedrati <92885675+Hindsedrati@users.noreply.github.com>
2023-01-28 14:24:28 +0100

    Update user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const validator = require('validator');
 
-router.post('/signup', userCtrl.signup);
+const password = require('../middleware/validator')
+
+
+router.post('/signup', password, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
